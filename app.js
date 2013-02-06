2a5a4942aa37988790c77369f879746b940cc918
Florian Sittel <sittel@lettis.net>
2013-02-06 17:47:18 +0100

    more refactoring

--- app.js
+++ app.js
@@ -32,69 +32,46 @@ app.configure('development', function(){
 });
 
 
+
+var objTypes = [ 'characters'
+                ,'combatskills'
+                ,'creatures'
+                ,'skills'
+                ,'spells' ];
+
 // JSON-DBs: dictionary with {name: path, ...} descriptions
 //           for JSON-files of Asgard objects
-var jsonDBs = (function(path){
-  var dbNames = [  'characters'
-                  ,'combatskills'
-                  ,'creatures'
-                  ,'skills'
-                  ,'spells' ];
-  return tools.hashify(
-           dbNames
-          ,dbNames.map(function(x){return path+x+".json";})
-  );
-})("./data");
-
-
+var jsonDBs = tools.hashify(
+                 dbNames
+                ,dbNames.map(
+                  function(x){return "./data/"+x+".json";}
+                 ) );
+ 
 app.get('/', routes.index);
 
-// character management
-app.get('/character', function(req, res){
-  Asgard.sendObjNames(jsonDBs['characters'], res);
+// define behavior for GET, PUT, POST & DELETE
+objTypes.map(function(objType){
+  // GET name list; e.g. '/character' -> character names
+  app.get('/'+objType, function(req, res){
+    Asgard.sendObjNames(jsonDBs[objType+'s'], res);
+  });
+  // GET single object
+  app.get('/'+objType+'/:id', function(req, res){
+    //TODO
+  });
+  // create single object (POST)
+  app.post('/'+objType+'/:id', function(req, res){
+    //TODO
+  });
+  // update single object (PUT)
+  app.put('/'+objType+'/:id', function(req, res){
+    //TODO
+  });
+  // DELETE single object
+  app.delete('/'+objType+'/:id', function(req, res){
+    //TODO
+  });
 });
-app.get   ('/character/:id' , character.get    );
-app.post  ('/character'     , character.create );
-app.put   ('/character/:id' , character.update );
-app.delete('/character/:id' , character.remove );
-
-// magic management
-app.get   ('/magic'     , magic.getList);
-app.get   ('/magic/:id' , magic.get    );
-app.post  ('/magic'     , magic.create );
-app.put   ('/magic/:id' , magic.update );
-app.delete('/magic/:id' , magic.remove );
-
-// skill management
-app.get   ('/skill'     , skill.getList);
-app.get   ('/skill/:id' , skill.get    );
-app.post  ('/skill'     , skill.create );
-app.put   ('/skill/:id' , skill.update );
-app.delete('/skill/:id' , skill.remove );
-
-// combat skill management
-app.get   ('/combatskill'     , combatskill.getList);
-app.get   ('/combatskill/:id' , combatskill.get    );
-app.post  ('/combatskill'     , combatskill.create );
-app.put   ('/combatskill/:id' , combatskill.update );
-app.delete('/combatskill/:id' , combatskill.remove );
-
-// armor management
-app.get   ('/armor'     , armor.getList);
-app.get   ('/armor/:id' , armor.get    );
-app.post  ('/armor'     , armor.create );
-app.put   ('/armor/:id' , armor.update );
-app.delete('/armor/:id' , armor.remove );
-
-// creatures
-app.get   ('/creature'     , creature.getList);
-app.get   ('/creature/:id' , creature.get    );
-app.post  ('/creature'     , creature.create );
-app.put   ('/creature/:id' , creature.update );
-app.delete('/creature/:id' , creature.remove );
-
-
-
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
